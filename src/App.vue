68b4e1400ae1020ccdd8cf31a97bd458f9107e6d
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2024-02-25 22:32:25 +0000

    fix: 🐛 render root view on preview and build mode

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { onMounted } from 'vue'
+onMounted(() => {
+  const router = useRouter()
+  router.push('/')
+})
+</script>
+
 <template>
   <router-view />
 </template>
